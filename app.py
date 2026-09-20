c92df8435e038428d9a6e06891dd95bab7a26327
karthi105 <118302628+karthi105@users.noreply.github.com>
2026-09-20 15:30:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ h1, h2, h3, p, label {
 """, unsafe_allow_html=True)
 
 st.markdown(
-    "<h2 style='white-space: nowrap; font-size: 36px;'>📹 Karthick's Lorex Assistant for Agents</h1>",
+    "<h0 style='white-space: nowrap; font-size: 36px;'>📹 Karthick's Lorex Assistant for Agents</h1>",
     unsafe_allow_html=True
 )
 st.markdown(
