8e5b42187677e54f0fe3b46708b67d341ba33a5b
karthi105 <118302628+karthi105@users.noreply.github.com>
2026-09-20 13:56:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ st.write("Select the customer issue to view the troubleshooting steps.")
 troubleshooting = {
 
     "Password reset": [
-        "Get the picture of live view with the date and time",
-        "Get the picture of info screen",
-        "Get the picture of device label",
-        "Reset the password using the device id and date in the the tool"
+        "Get the picture of live view with the date and time.",
+        "Get the picture of info screen.",
+        "Get the picture of device label.",
+        "Reset the password using the device id and date in the the tool."
     ]
 
     "Device removal": [
