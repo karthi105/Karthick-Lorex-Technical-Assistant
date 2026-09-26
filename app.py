989b502232647add99c8d5541b2a65e05af2ef93
karthi105 <118302628+karthi105@users.noreply.github.com>
2026-09-26 18:04:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ h1, h2, h3, p, label {
 """, unsafe_allow_html=True)
 
 st.markdown(
-    "<h1 style='white-space: nowrap; font-size: 72px;'>  Lorex Assistant for Agents</h1>",
+    "<h1 style='text-align: center; white-space: nowrap; font-size: 72px;'>Lorex Assistant for Agents</h1>",
     unsafe_allow_html=True
+)
 )
 st.markdown(
     """
